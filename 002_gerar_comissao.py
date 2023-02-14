7b11720bc33b68367e50da306bfd4f4e496e2a51
Gilvan Alves Amaral <amaralgilvan16@gmail.com>
2023-02-14 14:56:19 -0300

    14021530

--- 002_gerar_comissao.py
+++ 002_gerar_comissao.py
@@ -13,32 +13,26 @@ mycursor = mybd.cursor()
 if mybd.is_connected():
     print('conectado')
 
-# Processa calculo de comissão das viagens
 tabpd = ('select * from cte;')
 df = pd.read_sql(tabpd, mybd)
 tabcomissao = pd.DataFrame(df)
-# df = tabpd.set_index('numcte')
-
-#print(tabcomissao)
 
 lancar = 's'
 
 while True:
-    
+
     if lancar == 's':
         #
         cst_codmot = int(input('Digite o codigo do motorista: '))
-        tabpd = tabcomissao.loc[tabcomissao['codmot']== cst_codmot ]
-        #print(tabpd)
-        #
-        faturado = tabpd['v_frete'].sum ()
-        desp_viag = tabpd['V_desp_viagem'].sum ()
-        out_desp = tabpd['V_varla'].sum ()
-        diesel = tabpd['diesel'].sum ()
-        arla = tabpd['V_varla'].sum ()
+        tabpd = tabcomissao.loc[tabcomissao['codmot'] == cst_codmot]
+        
+        faturado = tabpd['v_frete'].sum()
+        desp_viag = tabpd['V_desp_viagem'].sum()
+        out_desp = tabpd['V_varla'].sum()
+        diesel = tabpd['diesel'].sum()
+        arla = tabpd['V_varla'].sum()
         combustivel = float(diesel + arla)
         comissao = faturado * 0.15
-
 
         gravar = "INSERT INTO comissao (codmot, faturado, combustivel,  desp_viag, out_desp, comissao) VALUES (%s,%s,%s,%s,%s,%s)"
         lista = (cst_codmot, faturado, combustivel, desp_viag, out_desp, comissao)
@@ -46,7 +40,7 @@ while True:
         mycursor.execute(gravar, lista)
         mybd.commit()
         print(mycursor.rowcount, "record inserted.")
-        lancar = str(input('Gerar comissão de outro motorista [s/n]? '))        
+        lancar = str(input('Gerar comissão de outro motorista [s/n]? '))
     else:
         print('Lançamento finalizado!')
         break
