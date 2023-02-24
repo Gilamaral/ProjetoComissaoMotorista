772c0dd5a21ef6cee1f15376827c5130132ebb2f
Gilvan Alves Amaral <amaralgilvan16@gmail.com>
2023-02-24 18:19:26 -0300

    24021820

--- visualisar_tab.py
+++
@@ -1,9 +0,0 @@
-import openpyxl as op
-import func_uteis as ft
-import pandas as pd
-
-
-tabpd = pd.read_excel('C:\ProjetosPython\ProjetoComissaoMotorista\BD_Excel\Comissão Motoristas.xlsx')
-tabpd = tabpd.set_index('numcte')
-
-print (tabpd)
